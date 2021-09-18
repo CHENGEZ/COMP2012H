1389e3d75a0b823b28e7290d9eba604189ffd704
CHENGEZ <1428624697@qq.com>
2021-09-18 21:39:52 +0800

    finished task1 and task2

--- PAs/PA1/game.cpp
+++ PAs/PA1/game.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+bool matches[HEIGHT][WIDTH] = {};
+
 void readMap(string filename, int map[][WIDTH])
 {
     // the map is 9 cells wide and 81 cells high, but only the bottom 9 rows cells are active.
@@ -89,8 +91,73 @@ bool inBounds(int y, int x)
  ***********************************************************************/
 int findMatches(int map[][WIDTH], bool matches[HEIGHT][WIDTH])
 {
+    int thisNumber = 0, nextNumber = 0;
+    int consecutiveCnt = 0, trueCnt = 0;
 
-    return 0;
+    // each time first set every position to be false
+    for (int rows = 0; rows <= HEIGHT - 1; rows++)
+    {
+        for (int cols = 0; cols <= WIDTH - 1; cols++)
+        {
+            matches[rows][cols] = false;
+        }
+    }
+
+    // check matches within one row
+    for (int y_cor = 0; y_cor <= HEIGHT - 1; y_cor++) // go through each row of the map
+    {
+        for (int x_cor = 0; x_cor <= WIDTH - 2; x_cor++) // go through each column of one row (except last column)
+        {
+            thisNumber = map[y_cor][x_cor];
+            nextNumber = map[y_cor][x_cor + 1];
+            if (nextNumber == thisNumber && thisNumber != 0)
+                consecutiveCnt++;
+            else
+                consecutiveCnt = 0;
+
+            if (consecutiveCnt >= 2)
+            {
+                matches[y_cor][x_cor - 1] = true;
+                matches[y_cor][x_cor] = true;
+                matches[y_cor][x_cor + 1] = true;
+            }
+        }
+    }
+
+    consecutiveCnt = 0;
+
+    // check matches within one colmun
+    for (int x_cor = 0; x_cor <= WIDTH - 1; x_cor++) // go through each column of the map
+    {
+        for (int y_cor = 0; y_cor <= HEIGHT - 2; y_cor++) // go through each row of one column (except top row)
+        {
+            thisNumber = map[y_cor][x_cor];
+            nextNumber = map[y_cor + 1][x_cor];
+            if (nextNumber == thisNumber && thisNumber != 0)
+                consecutiveCnt++;
+            else
+                consecutiveCnt = 0;
+
+            if (consecutiveCnt >= 2)
+            {
+                matches[y_cor - 1][x_cor] = true;
+                matches[y_cor][x_cor] = true;
+                matches[y_cor + 1][x_cor] = true;
+            }
+        }
+    }
+
+    // count how many trues have we got
+    for (int rows = 0; rows <= HEIGHT - 1; rows++)
+    {
+        for (int cols = 0; cols <= WIDTH - 1; cols++)
+        {
+            if (matches[rows][cols] == true)
+                trueCnt++;
+        }
+    }
+
+    return trueCnt;
 }
 
 /***********************************************************************
@@ -99,8 +166,28 @@ int findMatches(int map[][WIDTH], bool matches[HEIGHT][WIDTH])
  ***********************************************************************/
 int processMatches(int map[][WIDTH])
 {
+    int matchCntForThisSearch = 0, totalMatch = 0;
 
-    return 0;
+    while (findMatches(map, matches) != 0) // each time after shifting, entries shall be re-marked
+    {
+        matchCntForThisSearch = findMatches(map, matches);
+        totalMatch += matchCntForThisSearch;
+        for (int x_pos = 0; x_pos <= WIDTH - 1; x_pos++) // for each column
+        {
+            for (int y_pos = HEIGHT - 1; y_pos >= 0; y_pos--) // from up to down go through each row in one column
+            {
+                if (matches[y_pos][x_pos] == true) // if that entry was marked a match, shift the above down
+                {
+                    for (int index = y_pos; index <= MAX_ROWS - 2; index++)
+                    {
+                        map[y_pos][x_pos] = map[y_pos + 1][x_pos];
+                        map[MAX_ROWS - 1][x_pos] = 0;
+                    }
+                }
+            }
+        }
+    }
+    return totalMatch;
 }
 
 /***********************************************************************
