16af1f56f1a022f48f3806d451a013f2adad6b2d
CHENGEZ <1428624697@qq.com>
2021-10-05 16:29:15 +0800

    finish lab3 task 1 and 2

--- Labs/Lab3/EthernetFrame.cpp
+++ Labs/Lab3/EthernetFrame.cpp
@@ -7,29 +7,62 @@
 // TODO: task 1: initialize & swap EthernetFrame
 
 void initFrame(EthernetFrame &frame, const string &source, const string &destination,
-               const string &payload, EtherType type) {
+               const string &payload, EtherType type)
+{
+    frame.source = source;
+    frame.destination = destination;
+    frame.payload = payload;
+    frame.type = type;
     return;
 }
 
 void initFrame(EthernetFrame *frame, const string &source, const string &destination,
-               const string &payload, EtherType type) {
+               const string &payload, EtherType type)
+{
+    if (frame == nullptr)
+        return;
+    *frame.source = source;
+    *frame.destination = destination;
+    *frame.payload = payload;
+    *frame.type = type;
     return;
 }
 
-void swapFrames(EthernetFrame &frame1, EthernetFrame &frame2) {
+void swapFrames(EthernetFrame &frame1, EthernetFrame &frame2)
+{
+    EthernetFrame tempFrame = frame1;
+    frame1 = frame2;
+    frame2 = tempFrame;
     return;
 }
 
-void swapFrames(EthernetFrame *frame1, EthernetFrame *frame2) {
+void swapFrames(EthernetFrame *frame1, EthernetFrame *frame2)
+{
+    if (frame1 == nullptr || frame2 == nullptr)
+        return;
+    if (frame1 == frame2)
+        return;
+    EthernetFrame tempFrame = *frame1;
+    *frame1 = *frame2;
+    *frame2 = tempFrame;
     return;
 }
 
 // TODO: task 2: create & free EthernetFrame
 
-EthernetFrame *createFrame(const string &source, const string &destination, const string &payload, EtherType type) {
-    return nullptr;
+EthernetFrame *createFrame(const string &source, const string &destination, const string &payload, EtherType type)
+{
+    EthernetFrame *frame = new EthernetFrame;
+    *frame->destination = destination;
+    *frame->source = source;
+    *frame->payload = payload;
+    *frame->type = type;
+    return frame;
 }
 
-void freeFrame(EthernetFrame *frame) {
+void freeFrame(EthernetFrame *frame)
+{
+    delete frame;
+    frame = nullptr;
     return;
 }
