0a96df1023fe2246353dd2b01f8ee988f6481a85
CHENGEZ <1428624697@qq.com>
2021-10-06 13:48:43 +0800

    redo queueResizeRing() enqueue() and dequeue()

--- Labs/Lab3/Queue.cpp
+++ Labs/Lab3/Queue.cpp
@@ -10,59 +10,15 @@
 
 void queueResizeRing(Queue &queue, unsigned int newCapacity)
 {
-
-    EthernetFrame **newArr = new EthernetFrame *[newCapacity];
-
-    int shortage = 0;
-    int back = queue.capacity == 0 ? 0 : (queue.head + queue.size - 1) % queue.capacity;
-    int deleteCnt = 0, deleteIndex = 0;
-
-    if (newCapacity < queue.size)
-    {
-        shortage = queue.size - newCapacity;
-        while (deleteCnt < shortage)
-        {
-            deleteIndex = back - deleteCnt >= 0 ? back - deleteCnt : queue.capacity + (back - deleteCnt);
-            freeFrame(queue.ring[deleteIndex]);
-            deleteCnt++;
-        }
-    }
-
-    int newSize = newCapacity <= queue.size ? newCapacity : queue.size;
-    for (int i = 0; i < newSize; i++)
-    {
-        newArr[i] = queue.ring[(queue.head + i) % queue.capacity];
-    }
-
-    queue.ring = newArr;
-    queue.capacity = newCapacity;
-    queue.size = newSize;
-    queue.head = 0;
-
-    return;
+    
 }
 
 void enqueue(Queue &queue, EthernetFrame *frame)
 {
-    if (queue.size == queue.capacity)
-    {
-        queueResizeRing(queue, (queue.size + 1) * 2);
-    }
-    queue.ring[(queue.head + queue.size - 1) % queue.capacity] = frame;
-    queue.size++;
-    return;
 }
 
 void dequeue(Queue &queue)
 {
-    if (queue.size == 0)
-        return;
-    else
-    {
-        freeFrame(queue.ring[queue.head]);
-    }
-    queue.size--;
-    return;
 }
 
 const EthernetFrame *queueFront(const Queue &queue)
