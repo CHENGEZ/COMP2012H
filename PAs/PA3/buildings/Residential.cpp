09ce361b8881ea27638a6df3267a75c6e13cb68d
CHENGEZ <1428624697@qq.com>
2021-11-05 11:56:26 +0800

    re-design class structure to Category level

--- PAs/PA3/buildings/Residential.cpp
+++ PAs/PA3/buildings/Residential.cpp
@@ -10,8 +10,8 @@ Building::Category Residential::get_category() const
 int Residential::get_population() const { return this->population; }
 
 /*dummt implementation*/
-int Residential::get_revenue() const { return 0; };
-int Residential::get_max_population() const { return 0; };
-int Residential::get_population_growth() const { return 0; };
-int Residential::get_population_growth_rate_contribution() const { return 0; };
-void Residential::increase_population(int population) { population = 0; };
+int Residential::get_revenue() const { return 0; }
+int Residential::get_max_population() const { return 0; }
+int Residential::get_population_growth() const { return 0; }
+int Residential::get_population_growth_rate_contribution() const { return 0; }
+void Residential::increase_population(int population) { population = 0; }
