ae0f2354f53b01631cb28c3c5cbe4a3b1717ac37
CHENGEZ <1428624697@qq.com>
2021-11-10 13:32:33 +0800

    format lab7 before modifying

--- Labs/Lab7/my_unique_ptr.h
+++ Labs/Lab7/my_unique_ptr.h
@@ -4,14 +4,16 @@
 #include <utility>
 
 template <typename T>
-class my_unique_ptr {
+class my_unique_ptr
+{
 public:
 	/*******************************
 	 * Constructors and Destructor *
 	 *******************************/
 
 	// Default Constructor, p is empty.
-	my_unique_ptr() {
+	my_unique_ptr()
+	{
 		// TODO
 	}
 
@@ -20,69 +22,73 @@ public:
 	 * Note: There exists the issue of 2 my_unique_ptrs thinking they have single and separate ownership of the same object.
 	 * We ignore this for the sake of lab simplicity, as it is a complex problem to solve.
 	 */
-	my_unique_ptr(T* p) {
+	my_unique_ptr(T *p)
+	{
 		// TODO
 	}
 
 	// Copy Constructor - transfers ownership of object from u to this pointer
-	my_unique_ptr(my_unique_ptr<T>& x) {
+	my_unique_ptr(my_unique_ptr<T> &x)
+	{
 		// TODO
 	}
 
 	// Destructor - deallocate p if it is not a nullptr.
-	~my_unique_ptr() {
+	~my_unique_ptr()
+	{
 		// TODO
 	}
-
-
 
 	/************************
 	 * Operator Overloading *
 	 ************************/
 
 	// Copy Assignment Operator.
-	my_unique_ptr& operator=(my_unique_ptr<T>& x) {
+	my_unique_ptr &operator=(my_unique_ptr<T> &x)
+	{
 		// TODO
 	}
 
 	// Dereference Operators.
-	T& operator*() const {
+	T &operator*() const
+	{
 		// TODO
 	}
 
-	T* operator->() const {
+	T *operator->() const
+	{
 		// TODO
 	}
-
-
 
 	/*********************
 	 * Utility Functions *
 	 *********************/
 
-	T* get_pointer() const { return p; }
+	T *get_pointer() const { return p; }
 	bool is_empty() const { return p == nullptr; }
 
-	T* release() {
+	T *release()
+	{
 		// TODO
 	}
 
-	void reset() {
+	void reset()
+	{
 		// TODO
 	}
 
-	void reset(T* p) {
-		// TODO
-	}
-	
-	void swap(my_unique_ptr<T>& x) {
+	void reset(T *p)
+	{
 		// TODO
 	}
 
-
+	void swap(my_unique_ptr<T> &x)
+	{
+		// TODO
+	}
 
 private:
-	T* p;
+	T *p;
 };
 
 #endif /* MY_UNIQUE_PTR_H_ */
